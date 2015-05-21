50bd4bcd1b1bbd1dfb48d795a97fc4751a796ec3
orzcc <orzcczh@gmail.com>
2015-05-21 21:44:05 +0800

    兼容ocs特殊问题

--- src/AliyunOcsServiceProvider.php
+++ src/AliyunOcsServiceProvider.php
@@ -1,6 +1,7 @@
 <?php namespace Orzcc\AliyunOcs;
 
 use Illuminate\Support\ServiceProvider;
+use RuntimeException;
 use Cache;
 
 class AliyunOcsServiceProvider extends ServiceProvider {
@@ -9,14 +10,16 @@ class AliyunOcsServiceProvider extends ServiceProvider {
     {
         Cache::extend('ocs', function($app, $config)
         {
-            $memcached = $app['memcached.connector']->connect($config['servers']);
-            if(isset($config['servers']['authname']) && ini_get('memcached.use_sasl')) {
-                $user = $config['servers']['authname'];
-                $pass = $config['servers']['authpass'];
+            try {
+                $memcached = $app['memcached.connector']->connect($config['servers']);
+            } catch (RuntimeException $e) { // OCS可能会出现$memcached->getVersion()无法获取的问题
+            }
+           
+            $memcached->setOption(Memcached::OPT_COMPRESSION, false);
+            $memcached->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
 
-                $memcached->setOption(Memcached::OPT_COMPRESSION, false);
-                $memcached->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
-                $memcached->setSaslAuthData($user, $pass);
+            if(isset($config['servers']['authname']) && ini_get('memcached.use_sasl')) {
+                $memcached->setSaslAuthData($config['servers']['authname'], $config['servers']['authpass']);
             }
 
             $prefix = $app['config']['cache.prefix'];
