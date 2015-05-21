522f442a7eb1bcffbe045bb547cd323732cf585a
orzcc <orzcczh@gmail.com>
2015-05-21 23:00:09 +0800

    fix

--- src/AliyunOcsServiceProvider.php
+++ src/AliyunOcsServiceProvider.php
@@ -1,7 +1,6 @@
 <?php namespace Orzcc\AliyunOcs;
 
 use Illuminate\Support\ServiceProvider;
-use RuntimeException;
 use Cache;
 
 class AliyunOcsServiceProvider extends ServiceProvider {
@@ -10,10 +9,7 @@ class AliyunOcsServiceProvider extends ServiceProvider {
     {
         Cache::extend('ocs', function($app, $config)
         {
-            try {
-                $memcached = $app['memcached.connector']->connect($config['servers']);
-            } catch (RuntimeException $e) { // OCS可能会出现$memcached->getVersion()无法获取的问题
-            }
+            $memcached = $app['memcached.connector']->connect($config['servers']);
            
             $memcached->setOption(Memcached::OPT_COMPRESSION, false);
             $memcached->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
